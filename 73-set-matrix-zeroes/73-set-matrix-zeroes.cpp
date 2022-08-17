9da690eacd3d60ec2c48ab84e813d5a400ce116a
Yash Anand <76806913+0xYashAnand@users.noreply.github.com>
2022-08-17 17:16:01 +0530

    Time: 21 ms (60.98%), Space: 13.2 MB (82.05%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& m) {
-        vector<int> vi , vj;
+        vector<int> vi(m.size(),-1) , vj(m[0].size(),-1);
         for(int i=0; i<m.size(); i++){
             for(int j=0; j<m[i].size(); j++){
             if(m[i][j]==0) {
-                vi.push_back(i);
-                vj.push_back(j);
+                vi[i]=0;
+                vj[j]=0;
             }
          }
         }
-        for(int p=0; p<vi.size(); p++)
+        
          for(int i=0; i<m.size(); i++){
             for(int j=0; j<m[i].size(); j++){
-             if(i==vi[p] || j==vj[p]) m[i][j]=0;
+             if(vi[i]==0|| 0==vj[j]) m[i][j]=0;
           }
          }
     }
