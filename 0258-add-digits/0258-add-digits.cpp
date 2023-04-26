9a941a22beacebfca3d044b63220a696f9e9738f
Yash Anand <76806913+0xYashAnand@users.noreply.github.com>
2023-04-26 15:20:56 +0530

    Time: 3 ms (56.14%), Space: 5.9 MB (63.10%) - LeetHub

--- 0258-add-digits/0258-add-digits.cpp
+++ 0258-add-digits/0258-add-digits.cpp
@@ -4,16 +4,12 @@ public:
         int ans=0 ; 
         while(num>0){
             ans+=num%10;
-            //cout<<num<<endl;
             num/=10;
-           // cout<<num;
             if(num<10 && ans>9){
                 ans+=num%10;
-                //cout<<num<<" "<<ans<<endl;
                 num=ans;
                 ans=0;
             }
-            
         }
         
         return ans;
